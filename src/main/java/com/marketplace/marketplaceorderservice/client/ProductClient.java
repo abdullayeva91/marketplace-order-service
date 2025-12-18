c6739891f7a39eae773fee682287ec09279ff40a
laleabdullayeva <abdullayevala91@gmail.ru>
2025-12-18 21:15:41 +0400

    initial commit

--- src/main/java/com/marketplace/marketplaceorderservice/client/ProductClient.java
+++ src/main/java/com/marketplace/marketplaceorderservice/client/ProductClient.java
@@ -1,11 +1,15 @@
 package com.marketplace.marketplaceorderservice.client;
 
+import aQute.bnd.annotation.headers.Category;
 import com.marketplace.marketplaceorderservice.dto.ProductCreateRequest;
 import com.marketplace.marketplaceorderservice.dto.ProductResponse;
 import com.marketplace.marketplaceorderservice.dto.ProductUpdateRequest;
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @FeignClient(name = "product-service", url = "http://localhost:8082/api/products")
 public interface ProductClient {
@@ -19,4 +23,6 @@ public interface ProductClient {
     ProductResponse updateProduct(@PathVariable Long id, @Valid @RequestBody ProductUpdateRequest product);
     @DeleteMapping("/{id}")
     ProductResponse deleteProduct(@PathVariable Long id);
+    @GetMapping("/category/{category}")
+    ProductResponse getByCategory(@PathVariable Category category);
 }
